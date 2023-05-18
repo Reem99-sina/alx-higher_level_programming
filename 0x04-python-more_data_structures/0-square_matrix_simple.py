4aad9ba6290cb72a3be212bf69a43fb05410a44f
Reem99-sina <reemsina2@gmail.com>
2023-05-18 06:35:46 -0700

    -square_matrix_simpl

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    if not matrix:
+        return None
     inside_matrix=[[0 for i in range(len(matrix[0]))] for j in range(len(matrix))]
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
