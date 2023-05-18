ebb6b754339a0eb39f317eacd609d7887088e2c7
Reem99-sina <reemsina2@gmail.com>
2023-05-18 06:24:21 -0700

    -square_matrix_simpl

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    inside_matrix=[[0 for i in range(len(matrix[0]))] for j in range(len(matrix))]
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            inside_matrix[i][j]=matrix[i][j] ** 2
+    return inside_matrix
