409cd6341619fb187369684aa9513e8cafbef6e7
Reem99-sina <reemsina2@gmail.com>
2023-05-18 06:44:22 -0700

    -square_matrix_simpl

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,8 +2,4 @@
 def square_matrix_simple(matrix=[]):
     if not matrix:
         return None
-    inside_matrix=[[0 for i in range(len(matrix[0]))] for j in range(len(matrix))]
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            inside_matrix[i][j]=matrix[i][j] ** 2
-    return inside_matrix
+    return list(list(map(lambda a: a*a, num_list)) for num_list in matrix)
