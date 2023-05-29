cb60163da4094795e8452ac26c857e5722a14915
Reem99-sina <reemsina2@gmail.com>
2023-05-29 12:23:35 -0700

    print integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     try:
         integer=int(value)
-        print(value)
+        print("{:d}".format(value))
         return True
     except ValueError:
         return False
