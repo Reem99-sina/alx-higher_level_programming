dd7fb8b1ca4d284342fccd4d31b7f3436c092d14
Reem99-sina <reemsina2@gmail.com>
2023-05-30 14:43:54 -0700

    square print

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,27 +1,40 @@
 #!/usr/bin/python3
+"""Define classes for a singly-linked list."""
+
+
 class Node:
-    def __init__(self, data=0, next_node=None):
-        self.__data=data
-        self.__next_node=next_node
+    """Represent a node in a singly-linked list."""
+
+    def __init__(self, data, next_node=None):
+        """Initialize a new Node.
+        Args:
+            data (int): The data of the new Node.
+            next_node (Node): The next node of the new Node.
+        """
+        self.data = data
+        self.next_node = next_node
+
     @property
     def data(self):
+        """Get/set the data of the Node."""
         return (self.__data)
 
     @data.setter
     def data(self, value):
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
-        self.__datas = value
+        self.__data = value
+
     @property
     def next_node(self):
+        """Get/set the next_node of the Node."""
         return (self.__next_node)
+
     @next_node.setter
     def next_node(self, value):
-        if value is not None and not isinstance(value, Node):
+        if not isinstance(value, Node) and value is not None:
             raise TypeError("next_node must be a Node object")
-        else:
-            self.__next_node=value
-
+        self.__next_node = value
 class SinglyLinkedList:
     """Represent a singly-linked list."""
 
