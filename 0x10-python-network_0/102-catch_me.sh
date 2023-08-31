978182726093981ea76358b1e245c136a54721ff
Reem99-sina <reemsina2@gmail.com>
2023-08-31 08:43:12 -0700

    network

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-curl -s -X POST -d "You got me!" 0.0.0.0:5000/catch_me
+# Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
+curl -s  0.0.0.0:5000/catch_me | grep -o "You got me!"
