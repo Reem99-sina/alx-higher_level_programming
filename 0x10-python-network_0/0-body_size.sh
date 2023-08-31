ebcff0c6de237be942c83d24c63aaa8b079db789
Reem99-sina <reemsina2@gmail.com>
2023-08-31 08:12:52 -0700

    network

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-# Prompt the user to enter a URL
-read -p "Enter a URL: " url
-
 # Send a request to the URL and store the response in a variable
-response=$(curl -s -o /dev/null -w "%{size_download}" $url)
-
-# Display the size of the response body in bytes
-echo "Response size: $response bytes"
+curl -s "$1" | wc -c
